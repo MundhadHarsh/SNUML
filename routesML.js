4471432ecfa3750b379171dbc0506011499bc13f
Harsh Mundhada <harshmundhada@gmail.com>
2023-01-27 14:21:32 +0000

    First working model

--- routesML.js
+++ routesML.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router()
-import {predict } from "./ControllerML.js";
+import {predict1,predict2 } from "./ControllerML.js";
 
 
-router.get("/predict", predict)
+router.get("/predict1", predict1)
+router.post("/predict2", predict2)
 
 export default router 
--- script1.py
+++ script1.py
@@ -4,13 +4,15 @@ import traceback
 
 try:
     loaded_model = pickle.load(open("logistic1", 'rb'))
-    result = loaded_model.predict([[34,12,5940.83050148322]])
-    print('Resut: '+result+'-------------')
-except:
-    except Exception as e: print(e)
+    arg1=int(sys.argv[1])
+    arg2=int(sys.argv[2])
+    arg3=float(sys.argv[3])
 
 
-print('#Hello from python#')
-print('First param:'+sys.argv[1]+'#')
-# print('Second param:'+sys.argv[2]+'#')
+    result = loaded_model.predict([[arg1,arg2,arg3]])
+    print(result[0])
+except Exception as e: print(e)
+
+
+
 
